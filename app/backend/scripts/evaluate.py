1b23d65a8e7f879d652887f6920885982ffb4f6c
Artyom Bogachenco <artyombogachenco@gmail.com>
2020-12-06 20:39:23 +0300

    Fix args dashes

--- app/backend/scripts/evaluate.py
+++ app/backend/scripts/evaluate.py
@@ -3,17 +3,17 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fixed-acidity', type=float, help='g / dm^3')
-    parser.add_argument('--volatile-acidity', type=float, help='g / dm^3')
-    parser.add_argument('--citric-acid', type=float, help='g / dm^3')
-    parser.add_argument('--residual-sugar', type=float, help='g / dm^3')
-    parser.add_argument('--chlorides', type=float, help='g / dm^3')
-    parser.add_argument('--free-so2', type=float, help='g / dm^3')
-    parser.add_argument('--total-so2', type=float, help='g / dm^3')
-    parser.add_argument('--density', type=float, help='g / dm^3')
-    parser.add_argument('--ph', type=float)
-    parser.add_argument('--sulphates', type=float, help='g / dm^3')
-    parser.add_argument('--alc', type=float, help='g / dm^3')
+    parser.add_argument('fixed-acidity', type=float, help='g / dm^3')
+    parser.add_argument('volatile-acidity', type=float, help='g / dm^3')
+    parser.add_argument('citric-acid', type=float, help='g / dm^3')
+    parser.add_argument('residual-sugar', type=float, help='g / dm^3')
+    parser.add_argument('chlorides', type=float, help='g / dm^3')
+    parser.add_argument('free-so2', type=float, help='g / dm^3')
+    parser.add_argument('total-so2', type=float, help='g / dm^3')
+    parser.add_argument('density', type=float, help='g / dm^3')
+    parser.add_argument('ph', type=float)
+    parser.add_argument('sulphates', type=float, help='g / dm^3')
+    parser.add_argument('alc', type=float, help='g / dm^3')
     opt = parser.parse_args()
 
     predict = [[getattr(opt, arg) for arg in vars(opt)]]
